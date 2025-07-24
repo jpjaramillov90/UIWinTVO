f9b5d2df7a0f0a1efbedb0ad91811b40f3a9fa12
JoaoJV90 <joao.jaramillo@gmail.com>
2025-07-24 09:56:53 -0500

    change in start FRM

--- UIWinTVO/Program.cs
+++ UIWinTVO/Program.cs
@@ -13,7 +13,7 @@ namespace UIWinTVO
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new StartApp());//FRMLogin is the main form for the application
+            Application.Run(new FRMLogin());
         }
     }
 }
